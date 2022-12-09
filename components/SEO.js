b82a1a2525d86f06eb54c5319153d6352d2c34db
The Master Point <118052920+MasterPoint0@users.noreply.github.com>
2022-12-09 19:37:44 +0800

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -6,6 +6,7 @@ export default function SEO({ title, description }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
+      <link rel="icon" type="image/x-icon" href="https://cdn.discordapp.com/attachments/1040933844846641173/1042764310696427650/galaxytech_logo.png" />
     </Head>
   );
 }
